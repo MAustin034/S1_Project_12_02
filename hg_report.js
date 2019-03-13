d7597c34cf290a7cce0407a8e4aab98a6b139d7e
Mickal Austin <maustin034@west-mec.org>
2019-03-13 14:19:58 -0700

    code Update
    Was broken on submission but fixed errors

--- hg_report.js
+++ hg_report.js
@@ -14,34 +14,30 @@
 
 
 
-var gameReport = "<h1>" + itemTitle + "</h1><h2>By:" + itemManufacturer + "</h2><img src='hg_" + itemID + ".png'alt='id'id='gameImg'/><table><tr><th>Product ID</th><td>" + itemID + "</td></tr><tr><th>List Price</th><td>" + itemPrice + "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr><tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr><tr><th>Release</th><td>" + itemRelease + "</td></tr>"
+var gameReport = "<h1>" + itemTitle + "</h1>" + "<h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='id' id='gameImg' /> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
 
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
 
-gameReport += "</table>";
-gameReport += itemSummary;
-
-document.getElementById("article")[0].innerHTML = gameReport;
-
-var ratingsSum = 0;
+var ratingSum = 0;
 
 var ratingsCount = ratings.length;
 
 for (var i = 0; i < ratingsCount; i++) {
-    ratingsSum += ratings[i];
+    ratingSum += ratings[i];
 }
 
-var ratingAvg = ratingsSum / ratingsCount;
+var ratingsAvg = ratingSum / ratingsCount;
 
-var ratingReport = "<h1>Customer Reviews</h1>"
-ratingReport += "<h2>" + ratingAvg + "out of 5 stars(" + ratingsCount + "reviews)</h2>";
+var ratingReport = " <h1>Customer Reviews</h1> <h2>" + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
 
-for (var i = 0; i < 3; i++) {
-    ratingReport += " div class=”review”> <h1>" + ratingTitles[i] + "</h1><table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
-    for (var j = 0; j <= ratings[i]; j++) {
-        ;
+for (var j = 0; j <= 2; j++) {
+    ratingReport += "<div class='review'> <h1>" + ratingTitles[j] + "</h1><table> <tr><th>By</th><td>" + ratingAuthors[j] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[j] + "</td></tr> <tr><th>Rating</th><td>";
 
+    for (var k = 1; k <= ratings[j]; k++) {
+        ratingReport += "<img src='hg_star.png' />";
     }
 
+    ratingReport += "</td></tr></table>" + ratingSummaries[j] + "</div>";
 }
 
-document.getElementById("aside").innerHTML = ratingReport;
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
